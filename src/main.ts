66eaefb0336edf0308248e7ead6966765b355dbc
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-10-23 01:50:01 -0700

    added canvas

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import "./style.css";
 
 const APP_NAME = "Soladraw!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const canvas = document.createElement("canvas");
+canvas.width = 512;
+canvas.height = 512;
+document.body.append(canvas);
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const ctx = canvas.getContext("2d");
